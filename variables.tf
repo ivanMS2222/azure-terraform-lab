e3fcada39dd9370ac073ef6dda8f2fd5e67ecac8
ivanMS2222 <94165137+ivanMS2222@users.noreply.github.com>
2025-05-19 20:09:46 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,11 @@
+# This file declares the input variables used in the root module.
 
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region for resource deployment"
+  type        = string
+}

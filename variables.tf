feff943bcbd2fa613fe3487515d873b94e4f0a42
ivanms <94165137+ivanmorenosanz@users.noreply.github.com>
2025-05-19 20:32:48 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,34 @@
-# This file declares the input variables used in the root module.
+// Azure region for all resources
+variable "location" {
+  description = "Azure region where resources will be deployed"
+  type        = string
+  default     = "westeurope"
+}
 
+// Name of the resource group
 variable "resource_group_name" {
   description = "Name of the resource group"
   type        = string
+  default     = "rg-myproject"
 }
 
-variable "location" {
-  description = "Azure region for resource deployment"
+// VM name
+variable "vm_name" {
+  description = "Name of the virtual machine"
   type        = string
+  default     = "my-linux-vm"
+}
+
+// Admin username for the VM
+variable "admin_username" {
+  description = "Admin username for the VM"
+  type        = string
+  default     = "azureuser"
+}
+
+// Admin password for the VM (sensitive)
+variable "admin_password" {
+  description = "Admin password for the VM"
+  type        = string
+  sensitive   = true
 }

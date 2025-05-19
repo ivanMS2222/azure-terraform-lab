4cbb67e826816e4eea07bfcc059b352812962cca
ivanMS2222 <94165137+ivanMS2222@users.noreply.github.com>
2025-05-19 20:09:36 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,38 @@
+# This file orchestrates the Terraform modules for deploying Azure infrastructure.
+
+provider "azurerm" {
+  features {}
+}
+
+# Load sensitive variables from terraform.tfvars
+variable "resource_group_name" {}
+variable "location" {}
+
+# Create the resource group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Network module
+module "network" {
+  source              = "./modules/network"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  vnet_name           = "my-vnet"
+  subnet_name         = "my-subnet"
+  address_space       = ["10.0.0.0/16"]
+  subnet_prefix       = "10.0.1.0/24"
+}
+
+# Virtual Machine module
+module "vm" {
+  source              = "./modules/vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  subnet_id           = module.network.subnet_id
+  vm_name             = "my-vm"
+  admin_username      = "azureuser"
+  admin_password      = "P@ssword1234!" 
+}
 

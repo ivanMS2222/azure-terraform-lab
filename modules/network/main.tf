548e1b0d7768068307c277a4467af31616e8e957
ivanms <94165137+ivanmorenosanz@users.noreply.github.com>
2025-05-20 10:29:16 +0200

    Update main.tf

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -11,7 +11,3 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [var.subnet_prefix]
 }
-
-output "subnet_id" {
-  value = azurerm_subnet.subnet.id
-}

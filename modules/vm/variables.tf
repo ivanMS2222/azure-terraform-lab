7c794b8e999e9aa1a44e0408ada6e03f66a69538
ivanms <94165137+ivanmorenosanz@users.noreply.github.com>
2025-05-20 10:00:11 +0200

    Create variables.tf

---
+++ modules/vm/variables.tf
@@ -0,0 +1,28 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "vm_name" {
+  type = string
+}
+
+variable "admin_username" {
+  type = string
+}
+
+variable "admin_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "nsg_id" {
+  type = string
+}

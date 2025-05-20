b3e837b8122f38dd9fc1add8c523331fe39d3f11
ivanms <94165137+ivanmorenosanz@users.noreply.github.com>
2025-05-20 09:58:04 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,19 @@
+output "vm_name" {
+  value = module.vm.vm_name
+}
 
+output "subnet_id" {
+  value = module.network.subnet_id
+}
+
+output "nsg_id" {
+  value = module.nsg.nsg_id
+}
+
+output "storage_account_name" {
+  value = module.storage.storage_account_name
+}
+
+output "container_name" {
+  value = module.storage.container_name
+}

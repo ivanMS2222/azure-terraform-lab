9564d7f4c302d3af730e1d1ab8d3ec875e50e461
ivanms <94165137+ivanmorenosanz@users.noreply.github.com>
2025-05-20 09:55:39 +0200

    Update main.tf

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -1,14 +1,4 @@
-// Create a public IP for the virtual machine
-resource "azurerm_public_ip" "vm_public_ip" {
-  name                = "${var.vm_name}-public-ip"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  allocation_method   = "Dynamic"
-  sku                 = "Basic"
-}
-
-// Create a network interface (NIC) for the VM
-resource "azurerm_network_interface" "vm_nic" {
+resource "azurerm_network_interface" "nic" {
   name                = "${var.vm_name}-nic"
   location            = var.location
   resource_group_name = var.resource_group_name
@@ -17,40 +7,38 @@ resource "azurerm_network_interface" "vm_nic" {
     name                          = "internal"
     subnet_id                     = var.subnet_id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.vm_public_ip.id
   }
 }
 
-// Associate the NSG to the NIC
-resource "azurerm_network_interface_security_group_association" "vm_nic_nsg" {
-  network_interface_id      = azurerm_network_interface.vm_nic.id
+resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = var.nsg_id
 }
 
-// Create the virtual machine
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                  = var.vm_name
-  location              = var.location
-  resource_group_name   = var.resource_group_name
-  size                  = "Standard_B1s"
-  admin_username        = var.admin_username
-  admin_password        = var.admin_password
-  disable_password_authentication = false
-
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                = var.vm_name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = "Standard_B2s"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
   network_interface_ids = [
-    azurerm_network_interface.vm_nic.id
+    azurerm_network_interface.nic.id,
   ]
 
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
-    name                 = "${var.vm_name}-os-disk"
   }
 
   source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "20_04-lts-gen2"
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
     version   = "latest"
   }
+}
+
+output "vm_name" {
+  value = azurerm_windows_virtual_machine.vm.name
 }

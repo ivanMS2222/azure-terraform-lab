f129477052e3699705c420127fbf56725350fc06
ivanms <94165137+ivanmorenosanz@users.noreply.github.com>
2025-05-20 09:59:19 +0200

    Update outputs.tf

--- modules/nsg/outputs.tf
+++ modules/nsg/outputs.tf
@@ -1,6 +1,3 @@
-# This file exports the NSG ID for use in other modules
-
 output "nsg_id" {
-  description = "The ID of the created Network Security Group"
-  value       = azurerm_network_security_group.main.id
+  value = azurerm_network_security_group.nsg.id
 }

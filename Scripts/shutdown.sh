2d516ed65016fc7f223d44c05a295b18330a8c37
ivanms <ivanmorenosanz2@gmail.com>
2025-05-23 18:02:51 +0200

    shutdown script update

--- Scripts/shutdown.sh
+++ Scripts/shutdown.sh
@@ -1,6 +1,6 @@
 az login
-az vm deallocate --resource-group rg-lab-ivanms --name master-vm
-az vm deallocate --resource-group rg-lab-ivanms --name jenkins-vm
+az vm deallocate --resource-group rg-lab-ivanms --name vm-master
+az vm deallocate --resource-group rg-lab-ivanms --name jenkins-server
 az vm list -g rg-lab-ivanms --query "[?starts_with(name, 'target')].name" -o tsv | while read vmname; do
   az vm deallocate --resource-group rg-lab-ivanms --name "$vmname"
 done
